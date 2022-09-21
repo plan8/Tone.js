b3c2332232a331a4ec9bf88a5a1ca7445d745129
scazan <scottcazan@gmail.com>
2022-09-21 16:22:15 +0200

    add build to repo

---
+++ build/esm/classes.d.ts
@@ -0,0 +1,6 @@
+export * from "./core/index";
+export * from "./source/index";
+export * from "./signal/index";
+export * from "./instrument/index";
+export * from "./effect/index";
+export * from "./component/index";
---
+++ build/esm/source/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./UserMedia";
+export * from "./oscillator/Oscillator";
+export * from "./oscillator/LFO";
+export * from "./buffer/ToneBufferSource";
+export * from "./buffer/Player";
