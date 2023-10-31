85d8022cbfb8e99dc7938a9c91b746bb5c00c993
Andreas <andreas@plan8.se>
2023-10-31 13:40:19 +0100

    bitcrusher

--- Tone/effect/index.ts
+++ Tone/effect/index.ts
@@ -1,7 +1,7 @@
 // export * from "./AutoFilter";
 // export * from "./AutoPanner";
 // export * from "./AutoWah";
-//export * from "./BitCrusher";
+export * from "./BitCrusher";
 // export * from "./Chebyshev";
 export * from "./Chorus";
 // export * from "./Distortion";
